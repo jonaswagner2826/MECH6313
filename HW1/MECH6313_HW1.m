a372aa0cb13bc295a5639f18aab73056d517d834
jonaswagner2826 <47377464+jonaswagner2826@users.noreply.github.com>
2021-02-07 02:26:38 -0600

    Finished HW1

--- HW1/MECH6313_HW1.m
+++ HW1/MECH6313_HW1.m
@@ -3,7 +3,8 @@ clear
 close all
 
 pblm1 = false;
-pblm2 = true;
+pblm2 = false;
+pblm4 = true;
 
 if pblm1
 %% Problem 1
@@ -121,18 +122,105 @@ saveas(fig,fullfile([pwd '\\' 'HW1' '\\' 'fig'],'pblm2_vs_time_neg.png'))
 end
 
 
-
+if pblm4
 %% Problem 4
 
+syms f(x,r) g(x,r)
+
 % Problem 3.4.2
+sys342 = nlsys(@pblm342);
 
 
+r = linspace(-2,5,20);
+x = linspace(-4,4,20);
+[r_c,fig] = sys342.bifurcationPlot(r,x);
 
 
+xlimit = xlim;
+plot([xlimit(1),r_c],[0,0],'g');
+plot([r_c,xlimit(2)],[0,0],'g--');
+
+x = linspace(r_c,xlimit(2));
+y = 2 * sqrt(x-r_c);
+plot(x,y,'g-')
+plot(x,-y,'g-')
+hold off
+
+title('Problem 3.4.2')
+
+saveas(fig,fullfile([pwd '\\' 'HW1' '\\' 'fig'],'pblm4_342.png'))
 
 
+% Problem 3.4.4
+sys344 = nlsys(@pblm344);
+r = linspace(-5,3,20);
+x = linspace(-4,4,20);
+[r_c,fig] = sys344.bifurcationPlot(r,x);
 
 
+xlimit = xlim;
+plot([xlimit(1),r_c],[0,0],'g')
+plot([r_c,xlimit(2)],[0,0],'g--')
+
+x = linspace(xlimit(1),r_c);
+y = 1 * sqrt(abs(x-r_c));
+plot(x,y,'g--')
+plot(x,-y,'g--')
+hold off
+
+title('Problem 3.4.4')
+
+saveas(fig,fullfile([pwd '\\' 'HW1' '\\' 'fig'],'pblm4_344.png'))
+
+% Problem 3.4.7
+sys347 = nlsys(@pblm347);
+r = linspace(-5,20,25);
+x = linspace(-3,3,10);
+[r_c,fig] = sys347.bifurcationPlot(r,x);
+
+
+xlimit = xlim;
+x = linspace(r_c,xlimit(2));
+y = 0.35 * sqrt(x-r_c);
+plot(x,y,'g--')
+plot(x,-y,'g-')
+hold off
+
+title('Problem 3.4.7')
+
+saveas(fig,fullfile([pwd '\\' 'HW1' '\\' 'fig'],'pblm4_347.png'))
+
+% Problem 4.4.9
+sys349 = nlsys(@pblm349);
+r = linspace(-8,2,20);
+x = linspace(-4,4,20);
+[r_c,fig] = sys349.bifurcationPlot(r,x);
+
+
+xlimit = xlim;
+x = linspace(xlimit(1),-2);
+y = 0 * x;
+plot(x,y-1,'g--');
+plot(x,y+1,'g--');
+plot(x,y,'g-');
+
+x = linspace(-2,r_c);
+y = 0 * x;
+plot(x,y-x-1,'g--');
+plot(x,y+x+1,'g--');
+plot(x,y,'g-');
+
+x = linspace(r_c,xlimit(2));
+y = 0*x;
+plot(x,y,'g--');
+
+hold off
+
+title('Problem 3.4.9')
+
+saveas(fig,fullfile([pwd '\\' 'HW1' '\\' 'fig'],'pblm4_349.png'))
+
+end
 
 
 
@@ -197,24 +285,113 @@ function y = van_der_pol(x,u,parms)
     end
 end
 
-% function y = van_der_pol_neg(x,u,a)
-%     % VAD_DER_POL nonlin function
-%     arguments
-%         x (2,1) = [0; 0];
-%         u (1,1) = 0;
-%         a = -1;
-%     end
-%     
-%     % Array sizes
-%     n = 2; % Number of states
-%     p = 1; % Number of inputs
-% 
-%     % State Update Equations
-%     y(1,1) = x(2);
-%     y(2,1) = - a * (x(1)^2 -1) * x(2) - x(1) + u;
-%     
-%     
-%     if nargin ==0
-%         y = [n;p];
-%     end
-% end
+
+function y = pblm342(x,u,parms)
+    % VAD_DER_POL nonlin function
+    arguments
+        x (1,1) = 0;
+        u (1,1) = 0;
+        parms = false
+    end
+    
+    if parms == false
+        r = 1;
+    else
+        r = parms(1);
+    end
+    
+    % Array sizes
+    n = 1; % Number of states
+    p = 1; % Number of inputs
+
+    % State Update Equations
+    y(1,1) = r * x(1) - sinh(x(1));
+    
+    
+    if nargin ==0
+        y = [n;p];
+    end
+end
+
+
+function y = pblm344(x,u,parms)
+    % VAD_DER_POL nonlin function
+    arguments
+        x (1,1) = 0;
+        u (1,1) = 0;
+        parms = false
+    end
+    
+    if parms == false
+        r = 1;
+    else
+        r = parms(1);
+    end
+    
+    % Array sizes
+    n = 1; % Number of states
+    p = 1; % Number of inputs
+
+    % State Update Equations
+    y(1,1) = x(1) + (r*x(1))/(1+x(1)^2);
+    
+    
+    if nargin ==0
+        y = [n;p];
+    end
+end
+
+
+function y = pblm347(x,u,parms)
+    % VAD_DER_POL nonlin function
+    arguments
+        x (1,1) = 0;
+        u (1,1) = 0;
+        parms = false
+    end
+    
+    if parms == false
+        r = 1;
+    else
+        r = parms(1);
+    end
+    
+    % Array sizes
+    n = 1; % Number of states
+    p = 1; % Number of inputs
+
+    % State Update Equations
+    y(1,1) = 5 - r * exp(-x(1)^2);
+    
+    
+    if nargin ==0
+        y = [n;p];
+    end
+end
+
+function y = pblm349(x,u,parms)
+    % VAD_DER_POL nonlin function
+    arguments
+        x (1,1) = 0;
+        u (1,1) = 0;
+        parms = false
+    end
+    
+    if parms == false
+        r = 1;
+    else
+        r = parms(1);
+    end
+    
+    % Array sizes
+    n = 1; % Number of states
+    p = 1; % Number of inputs
+
+    % State Update Equations
+    y(1,1) = x + tanh(r*x(1));
+    
+    
+    if nargin ==0
+        y = [n;p];
+    end
+end
