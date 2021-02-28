e59791baaf9eb576b89001f88af0ef83519ed55e
jonaswagner2826 <47377464+jonaswagner2826@users.noreply.github.com>
2021-02-28 00:46:32 -0600

    HW2 - pblm 1 and 2

--- HW2/MECH6313_HW2.m
+++ HW2/MECH6313_HW2.m
@@ -8,169 +8,129 @@ pblm2 = true;
 
 if pblm1
 %% Problem 1
-parta = false;
-partb = false;
+% using ode 45 instead....
+parta = true;
+partb = true;
 partc = true;
 
 if parta
 %% Problem 1a
-
 % System Def
-% alpha < 0
-alpha = -1;
-sys1a_neg = nlsys(@pblm1a,'empty',0,-1,0,0,alpha);
-% alpha = 0
-alpha = 0;
-sys1a_zero = nlsys(@pblm1a,'empty',0,-1,0,0,alpha);
-% alpha > 0
-alpha = 1;
-sys1a_pos = nlsys(@pblm1a,'empty',0,-1,0,0,alpha);
-%Sys Array
-sysArray = [sys1a_neg, sys1a_zero, sys1a_pos];
+sys_func = @pblm1a;
+Parms = 0.1 * [-1, 1e-10, 1];
 
 % Simulation Setup
-N = 5e3;
-t_step = 0.01;
-t_max = N * t_step - t_step;
-T = reshape(0:t_step:t_max,N,1);
-U = 0 * T;
-
-% Phase Plot Comparrison
-fig = figure('position',[0,0,1500,500]);
-X_0 = [ -0.5,0.8, -1.5, 3;
-        0.5, -0.5, 2.7, -1.9];
+T = [0 100];
+X_0 = 0.5 * [1, 1, -1, -1; 1, -1, 1, -1];
+% X_0 = [ -0.5,0.8, -1.5, 3;
+%         0.5, -0.5, 2.7, -1.9];
 
 % Sim Phase Plots
+fig = figure('position',[0,0,1500,500]);
+N1 = size(Parms,2);
+N2 = size(X_0,2);
 simNum = 1;
-for i = 1:3
-    sys = sysArray(i);
-    ax = subplot(1,3,i);
-    for j = 1:4
-        SYS(simNum) = nlsim(sys,U,T,X_0(:,j));
-        SYS(simNum).phasePlot(1,2,'',fig,ax);
+for i = 1:N1
+    ax(i) = subplot(1,N1,i);
+    parms = Parms(i);
+    for j = 1:N2
+        [t,y] = ode45(@(t,y) sys_func(t,y,parms),T,X_0(:,j));
+        plot(y(:,1),y(:,2));
+        xlabel('x1')
+        ylabel('x2')
+        title(['\alpha = ', num2str(round(parms,3))])
         hold on
         simNum = simNum + 1;
     end
 end
+linkaxes(ax,'xy')
 
-subplot(1,3,1)
-title('$\alpha$ = -1','interpreter','latex')
-subplot(1,3,2)
-title('$\alpha$ = 0','interpreter','latex')
-subplot(1,3,3)
-title('$\alpha$ = 1','interpreter','latex')
-
-sgtitle('Problem 1a - Phase Portrait For Varyuing Alpha')
+sgtitle('Problem 1a - Phase Portrait For Varying Alpha')
 saveas(fig,fullfile([pwd '\\' 'HW2' '\\' 'fig'],'pblm1a.png'))
 
 end
 
 if partb
 %% Problem 1b
-
 % System Def
-% alpha < 0
-alpha = -1;
-sys1b_neg = nlsys(@pblm1b,'empty',0,-1,0,0,alpha);
-% alpha = 0
-alpha = 0;
-sys1b_zero = nlsys(@pblm1b,'empty',0,-1,0,0,alpha);
-% alpha > 0
-alpha = 1;
-sys1b_pos = nlsys(@pblm1b,'empty',0,-1,0,0,alpha);
-%Sys Array
-sysArray = [sys1b_neg, sys1b_zero, sys1b_pos];
+sys_func = @pblm1b;
+Parms = 0.1 * [-1, 1e-10, 1];
 
 % Simulation Setup
-N = 5e3;
-t_step = 0.01;
-t_max = N * t_step - t_step;
-T = reshape(0:t_step:t_max,N,1);
-U = 0 * T;
-
-% Phase Plot Comparrison
-fig = figure('position',[0,0,1500,500]);
-X_0 = [ -0.5,0.8, -0.5, 0.3;
-        -0.2, -0.5, 0.2, -0.9];
+T = [0 100];
+X_0 = 0.05 * [1, 1, -1, -1; 1, -1, 1, -1];
 
 % Sim Phase Plots
+fig = figure('position',[0,0,1500,500]);
+N1 = size(Parms,2);
+N2 = size(X_0,2);
 simNum = 1;
-for i = 1:3
-    sys = sysArray(i);
-    ax = subplot(1,3,i);
-    for j = 1:4
-        SYS(simNum) = nlsim(sys,U,T,X_0(:,j));
-        SYS(simNum).phasePlot(1,2,'',fig,ax);
+for i = 1:N1
+    ax(i) = subplot(1,N1,i);
+    parms = Parms(i);
+    for j = 1:N2
+        [t,y] = ode45(@(t,y) sys_func(t,y,parms),T,X_0(:,j));
+        plot(y(:,1),y(:,2));
+        xlabel('x1')
+        ylabel('x2')
+        title(['\alpha = ', num2str(round(parms,3))])
         hold on
         simNum = simNum + 1;
     end
 end
+linkaxes([ax(1),ax(2)],'xy')
 
-subplot(1,3,1)
-title('$\alpha$ = -1','interpreter','latex')
-subplot(1,3,2)
-title('$\alpha$ = 0','interpreter','latex')
-subplot(1,3,3)
-title('$\alpha$ = 1','interpreter','latex')
 
-sgtitle('Problem 1b - Phase Portrait For Varyuing Alpha')
+sgtitle('Problem 1b - Phase Portrait For Varying Alpha')
 saveas(fig,fullfile([pwd '\\' 'HW2' '\\' 'fig'],'pblm1b.png'))
 
 end
 
-
 if partc
 %% Problem 1c
-
 % System Def
-% alpha < 0
-alpha = -1;
-sys1c_neg = nlsys(@pblm1c,'empty',0,-1,0,0,alpha);
-% alpha = 0
-alpha = 0;
-sys1c_zero = nlsys(@pblm1c,'empty',0,-1,0,0,alpha);
-% alpha > 0
-alpha = 1;
-sys1c_pos = nlsys(@pblm1c,'empty',0,-1,0,0,alpha);
-%Sys Array
-sysArray = [sys1c_neg, sys1c_zero, sys1c_pos];
+sys_func = @pblm1c;
+Parms = 0.5 * [-1, 1];
 
 % Simulation Setup
-N = 5e3;
-t_step = 0.01;
-t_max = N * t_step - t_step;
-T = reshape(0:t_step:t_max,N,1);
-U = 0 * T;
-
-% Phase Plot Comparrison
-fig = figure('position',[0,0,1500,500]);
-X_0 = [ -0.5,0.8, -0.5, 0.3;
-        -0.2, -0.5, 0.2, -0.9];
+T = [0 10];
+X_0 = 0.5 * [1, 1, -1, -1; 1, -1, 1, -1];
 
 % Sim Phase Plots
+fig = figure('position',[0,0,1500,500]);
+N1 = size(Parms,2);
+N2 = size(X_0,2);
 simNum = 1;
-for i = 1:3
-    sys = sysArray(i);
-    ax = subplot(1,3,i);
-    for j = 1:4
-        SYS(simNum) = nlsim(sys,U,T,X_0(:,j));
-        SYS(simNum).phasePlot(1,2,'',fig,ax);
+for i = 1:N1
+    ax(i) = subplot(1,N1,i);
+    parms = Parms(i);
+    for j = 1:N2
+        [t,y] = ode45(@(t,y) sys_func(t,y,parms),T,X_0(:,j));
+        plot(y(:,1),y(:,2));
+        xlabel('x1')
+        ylabel('x2')
+        title(['\alpha = ', num2str(round(parms,3))])
         hold on
         simNum = simNum + 1;
     end
+    if ax(i).XLim(1) < -5
+        ax(i).XLim(1) = -5;
+    end
+    if ax(i).XLim(2) > 5
+        ax(i).XLim(2) = 5;
+    end
+    if ax(i).YLim(1) < -10
+        ax(i).YLim(1) = -5;
+    end
+    if ax(i).YLim(2) > 15
+        ax(i).YLim(2) = 15;
+    end
 end
 
-subplot(1,3,1)
-title('$\alpha$ = -1','interpreter','latex')
-subplot(1,3,2)
-title('$\alpha$ = 0','interpreter','latex')
-subplot(1,3,3)
-title('$\alpha$ = 1','interpreter','latex')
 
-sgtitle('Problem 1c - Phase Portrait For Varyuing Alpha')
+sgtitle('Problem 1c - Phase Portrait For Varying Alpha')
 saveas(fig,fullfile([pwd '\\' 'HW2' '\\' 'fig'],'pblm1c.png'))
 end
-    
 end
 
 if pblm2
@@ -179,28 +139,25 @@ parta = true;
 
 if parta
 %% Problem 2a
+disp('____________________  Problem 2:  ____________________')
 % sys def
-sys2a = nlsys(@pblm2a);
+sys2a = nlsys(@pblm2a)
 
 syms x1 x2
 linsys2a_sym = sys2a.linearize([x1;x2])
 linsys2a = sys2a.linearize([0;0])
 
 end
-
 end
-
-
-
 
 
 %% Local Functions
-function y = pblm1a(x,u,parms)
+function dx = pblm1a(t, x, parms)
     % pblm1a function
     arguments
+        t (1,1) = 0;
         x (2,1) = [0; 0];
-        u (1,1) = 0;
-        parms = false
+        parms = false;
     end
     
     if parms == false
@@ -208,26 +165,18 @@ function y = pblm1a(x,u,parms)
     else
         alpha = parms(1);
     end
-    
-    % Array Sizes
-    n = 2;
-    p = 1;
-    
+
     % State Upadate Eqs
-    y(1,1) = alpha * x(1) + x(2);
-    y(2,1) = - x(1) + alpha*x(2) - x(1)^2 * x(2);
-    
-    if nargin == 0
-        y = [n;p];
-    end
+    dx(1,1) = alpha * x(1) + x(2);
+    dx(2,1) = - x(1) + alpha*x(2) - x(1)^2 * x(2);
 end
 
-function y = pblm1b(x,u,parms)
-    % pblm1a function
+function y = pblm1b(t,x,parms)
+    % pblm1b function
     arguments
+        t (1,1) = 0;
         x (2,1) = [0; 0];
-        u (1,1) = 0;
-        parms = false
+        parms = false;
     end
     
     if parms == false
@@ -235,26 +184,18 @@ function y = pblm1b(x,u,parms)
     else
         alpha = parms(1);
     end
-    
-    % Array Sizes
-    n = 2;
-    p = 1;
     
     % State Upadate Eqs
     y(1,1) = alpha * x(1) + x(2) - x(1)^3;
     y(2,1) = - x(1) + alpha*x(2) + 2 *x(2)^2;
-    
-    if nargin == 0
-        y = [n;p];
-    end
 end
 
-function y = pblm1c(x,u,parms)
-    % pblm1a function
+function y = pblm1c(t,x,parms)
+    % pblm1c function
     arguments
+        t (1,1) = 0;
         x (2,1) = [0; 0];
-        u (1,1) = 0;
-        parms = false
+        parms = false;
     end
     
     if parms == false
@@ -262,18 +203,9 @@ function y = pblm1c(x,u,parms)
     else
         alpha = parms(1);
     end
-    
-    % Array Sizes
-    n = 2;
-    p = 1;
-    
     % State Upadate Eqs
     y(1,1) = alpha * x(1) + x(2) - x(1)^2;
-    y(2,1) = - x(1) + alpha*x(2) - x(1)^2 * x(1)^2;
-    
-    if nargin == 0
-        y = [n;p];
-    end
+    y(2,1) = - x(1) + alpha*x(2) + 2 * x(1)^2;
 end
 
 function y = pblm2a(x,u)
